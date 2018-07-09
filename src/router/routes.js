94b011f59644e1507c4800580dc3d7daa5a64675
闻佳 <wen_j@suixingpay.com>
2018-07-09 11:02:55 +0800

    first commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,16 @@
 import ReactLoadable from 'react-loadable';
 import LoadingPage from '../components/loading-page';
 
-const loadable = loader => ReactLoadable({loader, loading: LoadingPage});
-
-// import 引入不能是变量，这里无法用循环，只能每个路由都写import
-export default [
-    {path: '/', component: loadable(() => import('../pages/home/index'))},
-    {path: '/about', component: loadable(() => import('../pages/about/index'))},
-    {path: '/mine', component: loadable(() => import('../pages/mine/index'))},
-    {path: '/user', component: loadable(() => import('../pages/user/index'))},
+const routes = [
+    {path: '/', component: () => import('../pages/home/index')},
+    {path: '/about', component: () => import('../pages/about/index')},
+    {path: '/mine', component: () => import('../pages/mine/index')},
+    {path: '/user', component: () => import('../pages/user/index')},
 ];
+
+export default routes.map(item => {
+    return {
+        path: item.path,
+        component: ReactLoadable({loader: item.component, loading: LoadingPage}),
+    };
+});

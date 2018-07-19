4a3583a04fb44fb5031e7d013194f1d7f6d008c9
zkboys <876213774@qq.com>
2018-07-19 15:55:57 +0800

    异步级联下拉组件

---
+++ src/components/async-cascade-picker/index.jsx
@@ -0,0 +1,187 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Picker} from 'antd-mobile';
+
+/**
+ *
+ * 基于一组返回promise的方法，获取对应的 values labels dataSource
+ * 如果指定value与数据无法对应，将自动获取数据中的一个value
+ *
+ * @param value array
+ * @param dataSource array [{value, label, children: [{value, label}]}]
+ * @param promises array 异步请求
+ * @param useCache boolean 是否使用缓存
+ * @return {value, label, item, dataSource}
+ */
+function getAsyncDataSourceAndValue(value = [], dataSource = [], promises, useCache) {
+    return new Promise((resolve, reject) => {
+        if (!promises || !promises.length) return resolve(value, dataSource);
+        const allPromises = [...promises];
+        const resultValue = [...value];
+        const resultLabel = [];
+        const resultItem = [];
+
+        let firstPro = allPromises.shift();
+        if (dataSource && dataSource.length && useCache) {
+            firstPro = Promise.resolve(dataSource);
+        } else {
+            firstPro = firstPro().then(res => {
+                dataSource = res;
+                return dataSource;
+            });
+        }
+
+        const setResult = (res, i) => {
+            const firstValue = res[0].value;
+
+            let nextItem = res.find(item => item.value === resultValue[i]);
+
+            if (!nextItem) {
+                nextItem = res.find(item => item.value === firstValue);
+                resultValue[i] = firstValue;
+            }
+
+            resultLabel[i] = nextItem.label;
+            resultItem[i] = [...nextItem];
+        };
+
+        allPromises.reduce((prev, next, index) => {
+            return prev.then(res => {
+                if (!res || !res.length) return [];
+
+                const prevValue = resultValue[index];
+                const prevItem = res.find(item => item.value === prevValue);
+                const children = prevItem.children;
+
+                if (useCache && children && children.length) {
+                    setResult(children, index + 1);
+                    return children;
+                }
+
+                return next(prevValue, prevItem).then(nextRes => {
+                    if (!nextRes || !nextRes.length) return [];
+
+                    setResult(nextRes, index + 1);
+
+                    prevItem.children = nextRes;
+                    return nextRes;
+                });
+            })
+        }, firstPro.then(res => {
+            if (!res || !res.length) return [];
+
+            setResult(res, 0);
+
+            return res;
+
+        }))
+            .then(() => resolve({value: resultValue, label: resultLabel, item: resultItem, dataSource}))
+            .catch(reject);
+    });
+}
+
+/**
+ * 任意层级，异步级联下拉
+ */
+export default class AsyncCascadePicker extends Component {
+    constructor(props) {
+        super(props);
+        const {value,} = props;
+
+        this.state.value = value;
+        this.getAsyncResult(value).then(({item, dataSource}) => this.setState({item, dataSource}));
+
+    }
+
+    static propTypes = {
+        loadData: PropTypes.arrayOf(PropTypes.func),   // 函数数组，返回promise
+    };
+
+    static defaultProps = {
+        cols: 3,
+        value: [],
+    };
+
+    state = {
+        value: [],
+        label: [],
+        dataSource: [],
+    };
+
+    // componentWillReceiveProps 有可能会被频繁调用，异步返回时顺序无法保证，通过一个哨兵变量 count，确保使用最后一次异步
+    count = 0;
+
+    componentWillReceiveProps(nextProps) {
+        if ('value' in nextProps) {
+            let {value: nextValue} = nextProps;
+
+            if (!nextValue) nextValue = [];
+
+            this.count = this.count + 1;
+            const n = this.count;
+
+            // 需要根据nextValue获取异步数据，这里不能简单的 this.setState({value: nextValue});
+            this.getAsyncResult(nextValue).then(({value, label, item, dataSource}) => {
+                if (n < this.count) return; // 由于闭包，n会保持当时调用componentWillReceiveProps设置的值
+
+                this.setState({value, label, item, dataSource});
+            });
+        }
+    }
+
+    handlePickerChange = (value) => {
+        const {onPickerChange} = this.props;
+
+        if (onPickerChange) onPickerChange(value);
+
+        this.getAsyncResult(value).then(({value, label, item, dataSource}) => this.setState({value, label, item, dataSource}));
+    };
+
+    getAsyncResult(value) {
+        const {loadData, cols} = this.props;
+        const {dataSource} = this.state;
+        const promises = loadData.slice(0, cols);
+
+        return getAsyncDataSourceAndValue(value, dataSource, promises, true);
+    }
+
+    handleChange = () => {
+        const {value} = this.state;
+        const {onChange} = this.props;
+
+        if (onChange) {
+            this.getAsyncResult(value).then(({value, label}) => {
+                onChange(value, label);
+            });
+        }
+    };
+
+    handleDismiss = () => {
+        // 点击取消，恢复数据
+        const {onDismiss, value} = this.props;
+        if (onDismiss) onDismiss();
+        this.setState({value});
+    };
+
+    render() {
+        const {dataSource, value} = this.state;
+        const {
+            children,
+            loadData,
+            ...others,
+        } = this.props;
+
+        return (
+            <Picker
+                {...others}
+                value={value}
+                data={dataSource}
+                onPickerChange={this.handlePickerChange}
+                onChange={this.handleChange}
+                onDismiss={this.handleDismiss}
+            >
+                {children}
+            </Picker>
+        );
+    }
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
-import {Button} from 'antd-mobile';
+import {Button, List} from 'antd-mobile';
 import {Link} from 'react-router-dom';
 import NavBar from '../../components/nav-bar/index';
 import Carousel from '../../components/carousel/index';
-import ProvinceCityArea from '../../components/province-city-area';
+import ProvinceCityArea from '../../components/async-cascade-picker';
 // import TabBar from "../components/tabBar";
 import './index.less';
 
+const Item = List.Item;
+
 export default class Home extends Component {
+    state = {
+        value: [],
+        label: [],
+    };
+
     componentDidMount() {
         // console.log(this.props);
         // this.props.ajax
@@ -15,51 +22,106 @@ export default class Home extends Component {
         //     .then(res => {
         //         console.log(res);
         //     });
+        setTimeout(() => {
+            this.setState({provValue: ["420000", "5280"]})
+        }, 1000);
     }
 
     render() {
+        console.log('render provValue label', this.state.provValue, this.state.label);
+
         return (
             <div>
                 <Carousel data={
                     ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI']
                 }/>
 
-                <ProvinceCityArea
-                    onLoad={(value, label) => {
-                        // 首次初始化数据加载完成回调用，一般用于获取label，做回显时用
-                        console.log('onLoad:', value, label);
-                    }}
-                    value={['2', '22', '223']}
-                    onChange={(value, label) => {
-                        console.log('onChange:', value, label);
-                    }}
-                    loadProvince={() => {
-                        return Promise.resolve([
-                            {key: '1', value: '1北京'},
-                            {key: '2', value: '2上海'},
-                            {key: '3', value: '3广州'},
-                            {key: '4', value: '4深圳'},
-                        ]);
-                    }}
-                    loadCity={(prov) => {
-                        return Promise.resolve([
-                            {key: prov + '1', value: prov + '1市'},
-                            {key: prov + '2', value: prov + '2市'},
-                            {key: prov + '3', value: prov + '3市'},
-                            {key: prov + '4', value: prov + '4市'},
-                        ]);
-                    }}
-                    loadArea={(city) => {
-                        return Promise.resolve([
-                            {key: city + '1', value: city + '1区'},
-                            {key: city + '2', value: city + '2区'},
-                            {key: city + '3', value: city + '3区'},
-                            {key: city + '4', value: city + '4区'},
-                        ]);
-                    }}
-                >
-                    <div>省市区picker</div>
-                </ProvinceCityArea>
+                <List renderHeader={() => 'Basic Style'} className="my-list">
+                    <ProvinceCityArea
+                        value={this.state.value}
+                        onChange={(value, label) => {
+                            console.log('onChange:', value, label);
+                            this.setState({value, label});
+                        }}
+                        cols={3}
+                        loadData={[
+                            () => {
+                                return Promise.resolve([
+                                    {value: '1', label: '1北京'},
+                                    {value: '2', label: '2上海'},
+                                    {value: '3', label: '3广州'},
+                                    {value: '4', label: '4深圳'},
+                                ]);
+                            },
+                            (prov, provItem) => {
+                                console.log(prov, provItem);
+                                return Promise.resolve([
+                                    {value: prov + '1', label: prov + '1市'},
+                                    {value: prov + '2', label: prov + '2市'},
+                                    {value: prov + '3', label: prov + '3市'},
+                                    {value: prov + '4', label: prov + '4市'},
+                                ]);
+                            },
+                            (city) => {
+                                return Promise.resolve([
+                                    {value: city + '1', label: city + '1区'},
+                                    {value: city + '2', label: city + '2区'},
+                                    {value: city + '3', label: city + '3区'},
+                                    {value: city + '4', label: city + '4区'},
+                                ]);
+                            },
+                            (area) => {
+                                return Promise.resolve([
+                                    {value: area + '1', label: area + '1街'},
+                                    {value: area + '2', label: area + '2街'},
+                                    {value: area + '3', label: area + '3街'},
+                                    {value: area + '4', label: area + '4街'},
+                                ]);
+                            },
+                            (town) => {
+                                return Promise.resolve([
+                                    {value: town + '1', label: town + '1房'},
+                                    {value: town + '2', label: town + '2房'},
+                                    {value: town + '3', label: town + '3房'},
+                                    {value: town + '4', label: town + '4房'},
+                                ]);
+                            }
+
+                        ]}
+                    >
+                        <Item>Title</Item>
+                    </ProvinceCityArea>
+
+                    <ProvinceCityArea
+                        title="选择省市"
+                        onChange={(value, label) => {
+                            console.log('onchange:', value, label);
+                            this.setState({provValue: value, label});
+                        }}
+                        loadData={[
+                            () => this.props.ajax.post(`/front/auth/addressList?pid=0`, null, {headers: {'SXF-TOKEN': 'cd34a05fb9f7f573e736dbcda6a39d0c'}})
+                                .then((result) => {
+                                    const prov = (result && result.data && result.data.length) ? result.data : [];
+                                    return prov.map(item => ({value: item.key, label: item.value}));
+                                }),
+                            (prov) => this.props.ajax.get(`/front/auth/addressList?pid=${prov}`, null, {headers: {'SXF-TOKEN': 'cd34a05fb9f7f573e736dbcda6a39d0c'}})
+                                .then(result => {
+                                    const city = (result && result.data && result.data.length) ? result.data : [];
+                                    return city.map(item => ({value: item.key, label: item.value}));
+                                }),
+                        ]}
+                        cols={2}
+                        value={this.state.provValue}
+                        onOk={() => this.setState({provPickerVisible: false})}
+                    >
+                        <List.Item>
+                            居住地址{this.state.provValue}
+                        </List.Item>
+                    </ProvinceCityArea>
+
+                </List>
+
+
                 <Link to="/about">关于</Link>
                 <Link to="/user">用户</Link>
                 <div onClick={() => this.props.history.push('/mine')}>我的</div>
